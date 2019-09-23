8c139cf789c388b60e8ee85ec611f280ce34a233
834810071 <834810071@qq.com>
2019-09-23 16:08:43 +0800

    8

--- APUE/4-5.cpp
+++ APUE/4-5.cpp
@@ -6,22 +6,27 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <my_err.h>
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
-    if (open("tempfile", O_RDWR) < 0)
-    {
-        err_sys("open error");
-    }
-    
-    if (unlink("tempfile") < 0)
-    {
-        err_sys("unlink error");
-    }
-    
-    printf("file unlink\n");
-    sleep(15);
-    printf("done\n");
+//    if (open("tempfile", O_RDWR) < 0)
+//    {
+//        err_sys("open error");
+//    }
+//
+//    if (unlink("tempfile") < 0)
+//    {
+//        err_sys("unlink error");
+//    }
+//
+//    printf("file unlink\n");
+//    sleep(15);
+//    printf("done\n");
+
+    cout << getenv("/home/jxq/") << endl;
     
     exit(0);
 }

acbda48d701ebe05b87d59e0a804602aa64929f5
834810071 <834810071@qq.com>
2020-01-08 21:39:29 +0800

    1.7

--- c++11_14_17新增特性/test01.cpp
+++ c++11_14_17新增特性/test01.cpp
@@ -1,20 +1,52 @@
 //
-// Created by jxq on 19-12-27.
+// Created by jxq on 20-1-8.
 //
 
 #include <iostream>
+#include <map>
+#include <memory>
 
-class A final
+class ChatDialog
 {
+//其他实现省略...
+public:
+    ChatDialog()
+    {
+        std::cout << "ChatDialog constructor" << std::endl;
+    }
 
+    ~ChatDialog()
+    {
+        std::cout << "ChatDialog destructor" << std::endl;
+    }
+
+    void activate()
+    {
+        //实现省略
+    }
 };
 
-class B : A
-{
+//用于管理所有聊天对话框的map，key是好友id，value是ChatDialog是聊天对话框智能指针
+std::map<int64_t, std::unique_ptr<ChatDialog>> m_ChatDialogs;
 
-};
+//C++ 17 版本2
+void onDoubleClickFriendItem3(int64_t userid)
+{
+    //结构化绑定和try_emplace都是 C++17语法
+    auto spChatDialog = std::make_unique<ChatDialog>();
+    auto [iter, inserted] = m_ChatDialogs.try_emplace(userid, std::move(spChatDialog));
+    iter->second->activate();
+}
 
 int main()
 {
+    //测试用例
+    //906106643 是userid
+    onDoubleClickFriendItem3(906106643L);
+    //906106644 是userid
+    onDoubleClickFriendItem3(906106644L);
+    //906106643 是userid
+    onDoubleClickFriendItem3(906106643L);
+
     return 0;
 }

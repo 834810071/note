a968ba11a7ef1c6882b800681f6a002dcbe106eb
834810071 <834810071@qq.com>
2020-01-09 20:32:10 +0800

    1.10

--- c++11_14_17新增特性/test01.cpp
+++ c++11_14_17新增特性/test01.cpp
@@ -1,36 +1,108 @@
+#define READER_THREAD_COUNT 8
+#define LOOP_COUNT 5000000
+
 #include <iostream>
+#include <mutex>
+#include <shared_mutex>
 #include <thread>
-#include <zconf.h>
-#include <chrono>
 
 using namespace std;
 
-thread_local int g_mydata = 1;
-
-void thread_func1()
+class shared_mutex_counter
 {
-    while (true)
+public:
+    shared_mutex_counter() = default;
+
+    ~shared_mutex_counter() = default;
+
+    unsigned int get() const
     {
-        ++g_mydata;
+        shared_lock<shared_mutex> lock(mutex_);
+        return value_;
     }
-}
 
-void thread_func2()
-{
-    while (true)
+    void inscrement()
     {
-        cout << "g_mydata = " << g_mydata << ", ThreadID = " << this_thread::get_id() << endl;
-        this_thread::sleep_for(chrono::seconds(1));
+        unique_lock<shared_mutex> lock(mutex_);
+        ++value_;
     }
-}
 
-int main()
+    void reset()
+    {
+        unique_lock<shared_mutex> lock(mutex_);
+        value_ = 0;
+    }
+
+private:
+    mutable std::shared_mutex mutex_;
+    unsigned int value_ = 0;
+};
+
+class mutex_counter
 {
-    thread t1(thread_func1);
-    thread t2(thread_func2);
+    mutex_counter() = default;
 
-    t1.join();
-    t2.join();
-    return 0;
+    ~mutex_counter() = default;
+
+    unsigned int get() const
+    {
+        shared_lock<mutex> lock(mutex_);
+        return value_;
+    }
+
+    void inscrement()
+    {
+        unique_lock<mutex> lock(mutex_);
+        ++value_;
+    }
+
+    void reset()
+    {
+        unique_lock<mutex> lock(mutex_);
+        value_ = 0;
+    }
+
+private:
+    mutable std::mutex mutex_;
+    unsigned int value_ = 0;
+};
+
+void test_shared_mutex()
+{
+    shared_mutex_counter counter;
+    int temp;
+
+    auto write = [&counter]() {
+        for (int i = 0; i < LOOP_COUNT; ++i)
+        {
+            counter.inscrement();
+        }
+    };
+
+    auto reader = [&counter, &temp]() {
+        for (int i = 0; i < LOOP_COUNT; ++i)
+        {
+            temp = counter.get();
+        }
+    };
+
+    thread** tarray = new thread*[READER_THREAD_COUNT];
+    clock_t start = clock();
+
+    for (int i = 0; i < READER_THREAD_COUNT; ++i)
+    {
+        tarray[i] = new thread(reader);
+    }
+
+    thread tw(write);
+
+    for (int i = 0; i < READER_THREAD_COUNT; ++i)
+    {
+        tarray[i]->join();
+    }
+
+    tw.join();
+
+    clock_t end = clock();
+
 }
-

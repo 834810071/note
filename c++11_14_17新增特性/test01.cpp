19fa215a5351250bde797a7464bcab4860476efe
834810071 <834810071@qq.com>
2020-01-09 10:28:55 +0800

    1.9

--- c++11_14_17新增特性/test01.cpp
+++ c++11_14_17新增特性/test01.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
 #include <thread>
 #include <zconf.h>
+#include <chrono>
 
 using namespace std;
 
-int threadproc() {
+thread_local int g_mydata = 1;
+
+void thread_func1()
+{
     while (true)
     {
-        cout << "I am New Thread 1!" << endl;
+        ++g_mydata;
     }
 }
 
-int func() {
-    thread t(threadproc);
-    t.detach();
+void thread_func2()
+{
+    while (true)
+    {
+        cout << "g_mydata = " << g_mydata << ", ThreadID = " << this_thread::get_id() << endl;
+        this_thread::sleep_for(chrono::seconds(1));
+    }
 }
 
 int main()
 {
-    func();
-    while (true)
-    {
-        sleep(1000);
-    }
+    thread t1(thread_func1);
+    thread t2(thread_func2);
 
+    t1.join();
+    t2.join();
     return 0;
 }
+

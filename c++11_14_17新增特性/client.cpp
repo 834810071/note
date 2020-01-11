64c4d6d2a9eb6c422e126c806fb7435b0fb72a85
834810071 <834810071@qq.com>
2020-01-11 21:03:33 +0800

    4.2

---
+++ c++11_14_17新增特性/client.cpp
@@ -0,0 +1,57 @@
+//
+// Created by jxq on 20-1-10.
+//
+
+#include <iostream>
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <string>
+#include <cstring>
+#include <netinet/in.h>
+#include <zconf.h>
+#include <arpa/inet.h>
+
+using namespace std;
+
+int main()
+{
+    int clientfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (clientfd == -1)
+    {
+        cout << "create client socket error." << endl;
+        return -1;
+    }
+
+    struct sockaddr_in serveraddr;
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serveraddr.sin_port = htons(3000);
+    if (connect(clientfd, (struct sockaddr*)&serveraddr, sizeof serveraddr) == -1)
+    {
+        cout << "connect error." << endl;
+        return -1;
+    }
+
+    int ret = send(clientfd, "helloworld", strlen("helloworld"), 0);
+    if (ret != strlen("helloworld"))
+    {
+        cout << "send data error." << endl;
+        return -1;
+    }
+
+    cout << "send data successfully, data: " << "helloworld" << endl;
+    char recvbuf[32] = {0};
+    ret = recv(clientfd, recvbuf, 32, 0);
+    if (ret > 0)
+    {
+        cout << "recv data successfully, data: " << recvbuf << endl;
+    }
+    else
+    {
+        cout << "recv data error." << endl;
+    }
+    close(clientfd);
+
+    return 0;
+}
---
+++ c++11_14_17新增特性/server.cpp
@@ -0,0 +1,74 @@
+//
+// Created by jxq on 20-1-10.
+//
+
+#include <iostream>
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <string>
+#include <cstring>
+#include <netinet/in.h>
+#include <zconf.h>
+
+using namespace std;
+
+int main()
+{
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd == -1)
+    {
+        cout << "create listen socket error." << endl;
+        return -1;
+    }
+
+    struct sockaddr_in bindarr;
+    bindarr.sin_family = AF_INET;
+    bindarr.sin_addr.s_addr = htonl(INADDR_ANY);
+    bindarr.sin_port = htons(3000);
+
+    if (bind(listenfd, (struct sockaddr*)&bindarr, sizeof bindarr) == -1)
+    {
+        cout << "bind listen socket error." << endl;
+        return -1;
+    }
+
+    if (listen(listenfd, INTMAX_MAX) == -1)
+    {
+        cout << "listen error." << endl;
+        return -1;
+    }
+
+    while (true)
+    {
+        struct sockaddr_in clientaddr;
+        socklen_t clientaddrlen = sizeof clientaddr;
+        int clientfd = accept(listenfd, (struct sockaddr*)& clientaddr, &clientaddrlen);
+        if (clientfd != -1)
+        {
+            char recvbuf[32] = {};
+            int ret = recv(clientfd, recvbuf, 32, 0);
+            if (ret > 0)
+            {
+                cout << "recv data from client, data: " << recvbuf << endl;
+                ret = send(clientfd, recvbuf, strlen(recvbuf), 0);
+                if (ret != strlen(recvbuf))
+                {
+                    cout << "send data error." << endl;
+                }
+                else
+                {
+                    cout << "send to client successful, data : " << recvbuf << endl;
+                }
+            }
+            else
+            {
+                cout << "recv data error." << endl;
+            }
+            close(clientfd);
+        };
+    }
+    close(listenfd);
+
+
+    return 0;
+}
